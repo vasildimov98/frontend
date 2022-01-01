0b495808b32a19f918f2d7641f05f55cf953b5cc
Vasil Dimov <vasil.dimov@gtmhub.com>
2022-01-01 22:14:16 +0200

    AngularJS Anchorscroll-service

---
+++ angularjs/angularjs-anchorscroll-service/scripts/script.js
@@ -0,0 +1,9 @@
+var demoApp = angular
+  .module("demoApp", [])
+  .controller("demoController", function ($scope, $location, $anchorScroll) {
+    $scope.scrollTo = function (scrollLocation) {
+      $location.hash(scrollLocation);
+      $anchorScroll.yOffset = 20;
+      $anchorScroll();
+    };
+  });
